c16d611613aeaeea95c098d6f1aa837c88106654
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-02 21:14:38 +0600

    03_reverseString exercise solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text = '') {
+    letters = [];
+    revText = '';
+  
+    for (letter of text){
+      letters.push(letter);
+    }
+  
+    while(letters.length>0){
+      revText += letters.pop();
+  
+    }
+  
+    return revText;
 
 };
 
