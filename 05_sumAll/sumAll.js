9c4c246d8808a25350972530176d1b8667f93865
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-02 23:20:16 +0600

    05_sumAll exercise submission

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start = 0, end = 0) {
+
+    let sum = 0;
+    let i = 0;
+
+    if(!Number.isInteger(start) || !Number.isInteger(end)){return 'ERROR';}
+    
+    if(typeof(start)!=='number' || typeof(end)!=='number'){return 'ERROR';}
+
+    if (start < 0 || end  < 0) {return 'ERROR'};
+
+    if (start > end){
+        start = start + end;
+        end = start - end;
+        start = start - end;
+    }
+
+    for(i=start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
