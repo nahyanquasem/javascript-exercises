4fa62d75b69039fb4d1babccf5ed30f551693115
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-02 20:59:18 +0600

    02_repeatString exercise solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text = '', count=0) {
+    
+    if(count===-1) return 'ERROR';
+
+    
+    let newtext = '';
+    let i = 0;
+
+    for(i=0; i<count; i++){
+        newtext = newtext + text;
+    }
+
+    return newtext;
 
 };
 
