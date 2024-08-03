83ced7cc3a43bc265110a0f824e5b21bff4d6774
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-03 11:02:46 +0600

    06_leapYears exercise submission

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year = 0) {
+
+    let divBy4 = year % 4;
+    let divBy100 = year % 100;
+    let divBy400 = year % 400;
+
+    if(divBy4!==0) {return false;}
+    else if (divBy100!==0) {return true;}
+    else if (divBy400===0) {return true;}
+    else return false;
+    
 
 };
 
