f8f5cea50dfafee56386948788f6d59658e27f1a
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-03 11:57:13 +0600

    07_tempConversion exercise submission

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(tempF=0.0) {
+
+  return round((tempF-32) * (5/9), 1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC=0.0) {
+
+  return round((tempC * (9/5) + 32),1);
+
 };
 
 // Do not edit below this line
