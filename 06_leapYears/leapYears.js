f6d9e832bb99b98fa9d3608133c5cf3a0a8f8586
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-03 11:12:02 +0600

    06_leapYears better solution submission

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,15 @@
 const leapYears = function(year = 0) {
 
-    let divBy4 = year % 4;
-    let divBy100 = year % 100;
-    let divBy400 = year % 400;
+    let divBy4 = year % 4 === 0;
+    let divBy100 = year % 100 === 0;
+    let divBy400 = year % 400 === 0;
 
-    if(divBy4!==0) {return false;}
-    else if (divBy100!==0) {return true;}
-    else if (divBy400===0) {return true;}
-    else return false;
+    // if(divBy4!==0) {return false;}
+    // else if (divBy100!==0) {return true;}
+    // else if (divBy400===0) {return true;}
+    // else return false;
+
+    return (divBy4) && (!divBy100  || divBy400);
     
 
 };
