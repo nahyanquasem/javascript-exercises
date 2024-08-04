5399300a175077931c5569f353d66319a809963f
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-04 15:15:25 +0600

    04_removeFromArray exercise solution (peeked)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-    
+
+const removeFromArray = function(list=[], ...args) {
+
+    const newlist = [];
+
+    list.forEach((item) => { //for every item in list
+
+        if(!args.includes(item)){ //if a particular item is not found in the args array
+            newlist.push(item); //then push that item into the new list
+        }
+
+    });
+
+    return newlist;
+  
 };
 
 // Do not edit below this line
